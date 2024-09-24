df7e5b4c60de7131187c7fc7964dff17d6e4e820
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-09-24 15:25:18 +0200

    [exp09] Print stderr before checking exit code

--- exp/09-ivp-burgers-native-vs-oif/run_rhs_eval.py
+++ exp/09-ivp-burgers-native-vs-oif/run_rhs_eval.py
@@ -12,20 +12,20 @@ p1 = subprocess.run(
     encoding="utf-8",
     capture_output=True,
 )
-assert p1.returncode == 0
 print(p1.stdout)
 if p1.stderr:
     print(p1.stderr)
+assert p1.returncode == 0
 
 print()
 
 p2 = subprocess.run(
     ["julia", EXPDIR / "call_rhs_eval_julia.jl"], encoding="utf-8", capture_output=True
 )
-assert p2.returncode == 0
 print(p2.stdout)
 if p2.stderr:
     print(p2.stderr)
+assert p2.returncode == 0
 
 with open(RESULT_RHS_EVALS_FILENAME, "w") as fh:
     fh.write(p1.stdout)
