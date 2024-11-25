1fff1c0b80c0b0f1f030e25a058337e4d55e173a
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-11-25 18:14:02 +0100

    [exp12] Create also a symlink to _assets subdirectories along with note.md

--- link_note.py
+++ link_note.py
@@ -6,8 +6,12 @@ p = argparse.ArgumentParser()
 p.add_argument("dirname", help="Dirname of the experiment to copy")
 args = p.parse_args()
 
+rootdir = os.getcwd()
+bookdir = Path("oif-notebook")
+
 dirname = Path(args.dirname)
 note = dirname / "note.md"
+assetsdir = dirname / "_assets"
 
 if not os.path.isdir(dirname):
     raise RuntimeError("Argument must be a directory for computational experiment")
@@ -15,9 +19,10 @@ if not os.path.isdir(dirname):
 if not os.path.isfile(note):
     raise RuntimeError(f"File '{note.as_posix():s}' does not exist")
 
+if not os.path.isdir(assetsdir):
+    raise RuntimeError(f"Directory '{assetsdir.as_posix():s}' does not exist")
 
-rootdir = os.getcwd()
-bookdir = Path("oif-notebook")
 
 os.makedirs(bookdir / dirname, exist_ok=True)
 os.symlink(rootdir / note, bookdir / note)
+os.symlink(rootdir / assetsdir, bookdir / assetsdir)
