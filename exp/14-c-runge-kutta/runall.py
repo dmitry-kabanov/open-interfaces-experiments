f468d9dd34bd95741f471201a576bd459d13e20b
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-01-06 17:55:13 +0100

    [exp14] Run solvers multiple times for different resolutions and compute statistics

--- exp/14-c-runge-kutta/runall.py
+++ exp/14-c-runge-kutta/runall.py
@@ -1,57 +1,112 @@
 #!/usr/bin/env python
 """Run OIF and raw versions of Burgers' solver multiple time and compute statistics."""
+import csv
+import os
 import subprocess
 
 import numpy as np
 
-from helpers import get_expdir, get_outdir
+from helpers import compute_mean_and_ci, get_expdir, get_outdir
 
 OUTDIR = get_outdir()
 EXPDIR = get_expdir()
 
-RESULTS_OIF_FILENAME = OUTDIR / "oif.txt"
-RESULTS_RAW_FILENAME = OUTDIR / "raw.txt"
+FLAG_COMPLETED = OUTDIR / "completed"
+RESULTS_PERF_FILENAME = "_assets/c-oif-vs-raw.csv"
 
-RESOLUTION_LIST = [200, 400, 800, 1600, 3200, 6400]
+RESOLUTIONS_LIST = [200, 400, 800, 1600, 3200, 6400]
 
-
-def _get_runtimes(version: str) -> list[float]:
-    assert version in ["oif", "raw"]
-
-    runtimes = []
-    for N in RESOLUTION_LIST:
-        p_oif = subprocess.run(
-            [f"./run_burgers_{version}", str(N)],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            universal_newlines=True,
-        )
-        print(p_oif.stdout)
-        assert p_oif.returncode == 0
-        for line in p_oif.stdout.split("\n"):
-            if line.startswith("Elapsed time ="):
-                chunks = line.split()
-                runtime = float(chunks[-2])
-                assert runtime > 0
-                break
-        runtimes.append(runtime)
-        print()
-
-    return runtimes
+N_RUNS = 30
+VERSIONS = ["oif", "raw"]
 
 
-def _compute()
-    runtimes_oif = _get_runtimes("oif")
-    runtimes_raw = _get_runtimes("raw")
+def _get_result_filename(N: int, version: str) -> str:
+    dirname = OUTDIR / f"N={N:04d}"
+    return str(dirname / f"{version}.txt")
 
-    np.savetxt(RESULTS_OIF_FILENAME, runtimes_oif)
-    print(f"Saved runtime results for OIF in {RESULTS_OIF_FILENAME}")
 
-    np.savetxt(RESULTS_RAW_FILENAME, runtimes_raw)
-    print(f"Saved runtime results for RAW in {RESULTS_RAW_FILENAME}")
+def main():
+    if not os.path.isfile(FLAG_COMPLETED):
+        _compute()
+        open(FLAG_COMPLETED, "a")
+    else:
+        _process()
+
+
+def _compute():
+    runtimes_table_oif = _get_runtimes("oif")
+    runtimes_table_raw = _get_runtimes("raw")
+
+    for N in RESOLUTIONS_LIST:
+        print(f"Resolution N = {N:04d}")
+        os.makedirs(OUTDIR / "N={N:04d}".format(N=N), exist_ok=True)
+
+        fn = _get_result_filename(N, "oif")
+        np.savetxt(fn, runtimes_table_oif[N])
+        print(f"Saved runtime results for OIF in {fn}")
+
+        fn = _get_result_filename(N, "raw")
+        np.savetxt(fn, runtimes_table_raw[N])
+        print(f"Saved runtime results for RAW in {fn}")
+
+
+def _get_runtimes(version: str) -> dict:
+    assert version in VERSIONS
+
+    runtimes_table = {}
+    for N in RESOLUTIONS_LIST:
+        runtimes = []
+        for _ in range(N_RUNS):
+            p_oif = subprocess.run(
+                [f"./run_burgers_{version}", str(N)],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+            )
+            print(p_oif.stdout)
+            assert p_oif.returncode == 0
+            for line in p_oif.stdout.split("\n"):
+                if line.startswith("Elapsed time ="):
+                    chunks = line.split()
+                    runtime = float(chunks[-2])
+                    assert runtime > 0
+                    break
+            runtimes.append(runtime)
+            print()
+
+        runtimes_table[N] = runtimes
+
+    return runtimes_table
+
 
 def _process():
-    runtimes_oif = np.loadtxt(RESULTS_OIF_FILENAME)
-    runtimes_raw = np.loadtxt(RESULTS_RAW_FILENAME)
+    table = {}
+    for v in VERSIONS:
+        table[v] = []
 
-    mean_oif, ci_oif
+    for N in RESOLUTIONS_LIST:
+        print()
+        print(f"Resolution N = {N}")
+
+        runtimes = {}
+        runtimes["oif"] = np.loadtxt(_get_result_filename(N, "oif"))
+        runtimes["raw"] = np.loadtxt(_get_result_filename(N, "raw"))
+
+        print()
+        for v in VERSIONS:
+            mean, ci = compute_mean_and_ci(runtimes[v])
+            val = f"{mean:.2f} ± {ci:.2f}"
+            table[v].append(val)
+
+    with open(RESULTS_PERF_FILENAME, "w") as fh:
+        writer = csv.writer(fh)
+        writer.writerow(["# method"] + RESOLUTIONS_LIST)
+        for v in VERSIONS:
+            writer.writerow(["{:30s}".format(v)] + table[v])
+
+    print(f"Results are written to file '{RESULTS_PERF_FILENAME}'")
+    subprocess.run(["column", "-s,", "-t", RESULTS_PERF_FILENAME])
+
+
+if __name__ == "__main__":
+    main()
