f6a0a324cf047a5c0dda6ad0920db38b1e4a546f
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-01-21 14:38:32 +0100

    [exp14] Update push-to-server script

--- exp/14-c-runge-kutta/push-to-server.sh
+++ exp/14-c-runge-kutta/push-to-server.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-
-# Check if rsync is installed
-if ! command -v rsync &> /dev/null; then
-    echo "Error: rsync is not installed."
-    exit 1
-fi
-
-# Define the target server and base path
+# Define the target server and the base path
 SERVER="myri"
-TARGET_BASE_PATH="~/target/directory" # Update this path to the base directory on the server
-
-dirname=${PWD/#$HOME/}
+dirname=${PWD/#$HOME\/}
 
 # Exclude patterns
 EXCLUDE=(
     "--exclude=_output/"
     "--exclude=_assets/"
-    "--exclude=*.sh"
+    "--exclude=code/build*/"
 )
 
 # Run rsync
-rsync -avz --progress "${EXCLUDE[@]}" ./ "${SERVER}:${dirname}"
-
-# Confirm completion
-if [ $? -eq 0 ]; then
+if rsync -avz --delete --progress "${EXCLUDE[@]}" ./ "${SERVER}:${dirname}" ; then
     echo "Files successfully copied to ${SERVER}:${dirname}."
     echo "Do not forget to do \`make clean\` and \`make\` to recompile"
 else
